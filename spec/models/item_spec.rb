efbd0cce588e4b2ea137c7bbfc38816e429ee3fb
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-08 12:10:51 -0700

    Feat: add association for Item. Item model association tested

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of :description }
     it { should validate_presence_of :unit_price }
     it { should validate_presence_of :merchant_id }
+    it { should have_many(:discounts).through(:merchant) }
   end
   describe "relationships" do
     it { should have_many(:invoices).through(:invoice_items) }
