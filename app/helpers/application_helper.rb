870bbfe95aa7161f61cf760cdb0a671e9b903ce7
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-09 07:39:17 -0700

    Refactor: Get coverage to 100%

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,6 @@
 module ApplicationHelper
 
   def monetize(number)
-    if number == 0
-      return "$0.00"
-    end
     string_num = number.to_s.insert(-3, "")
     number_to_currency(string_num, precision: 2)
   end
